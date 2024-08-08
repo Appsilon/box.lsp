2f3f5d5566d56b6c492801c63fd5c3867f3936db
Rodrigo Basa <rodrigo@appsilon.com>
2024-08-08 17:23:31 +0800

    new use_box_lsp function

---
+++ R/utils.R
@@ -0,0 +1,20 @@
+#' @export
+use_box_lsp <- function(file_path = ".Rprofile") {
+  rprofile <- fs::path_package("box.lsp", "Rprofile.R")
+  to_write <- readLines(rprofile)
+
+  tryCatch({
+    if (file.exists(file_path)) {
+      cli::cli_alert_info(
+        "{file_path} exists. Will append {{box.lsp}} configuration lines to the file."
+      )
+      write("", file = file_path, append = TRUE)
+    }
+    write(to_write, file = file_path, append = TRUE)
+    cli::cli_alert_success(
+      "{{box.lsp}} configuration written to {file_path}. Please check the changes made."
+    )
+  }, error = function(e) {
+    cli::cli_abort(e)
+  })
+}
