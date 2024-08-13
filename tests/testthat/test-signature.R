3000e3e649262e33ba7e561557ecab2494d6695b
Rodrigo Basa <rodrigo@appsilon.com>
2024-08-13 11:31:59 +0800

    tests skipped for various reasons

---
+++ tests/testthat/test-signature.R
@@ -0,0 +1,27 @@
+test_that("signature works with box-attached functions", {
+  skip("SKIP. Erratic, inconsistent.")
+  client <- language_client()
+
+  temp_file <- withr::local_tempfile(fileext = ".R")
+  writeLines(
+    c(
+      "box::use(stringr[str_count, str_match])",
+      "str_count(",
+      "str_match('foo', "
+    ),
+    temp_file
+  )
+
+  client %>% did_save(temp_file)
+
+  result <- client %>% respond_signature(temp_file, c(1, 10),
+                                         retry_when = function(result) result$signatures %>% length() == 0)
+  expect_length(result$signatures, 1)
+  expect_match(result$signatures[[1]]$label, "str_count\\(.*")
+  expect_equal(result$signatures[[1]]$documentation$kind, "markdown")
+
+  result <- client %>% respond_signature(temp_file, c(2, 17))
+  expect_length(result$signatures, 1)
+  expect_match(result$signatures[[1]]$label, "str_match\\(.*")
+  expect_equal(result$signatures[[1]]$documentation$kind, "markdown")
+})
